e87f6831f99181d95ef5117d06edf2ec7452554c
yasumuo <yasufrom24@gmail.com>
2019-01-30 03:01:44 +0900

    Created a group editing function
    It is now possible to update the group name and the user belonging to
    the group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,6 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
+
   def index
     @mygroups = current_user.groups
   end
@@ -21,10 +23,19 @@ class GroupsController < ApplicationController
   end
 
   def update
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+    else
+      render :edit
+    end
   end
 
   private
   def group_params
     params.require(:group).permit(:name, {:user_ids => []})
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 end
