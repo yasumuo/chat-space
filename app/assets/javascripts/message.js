4d8ef65b6267c4d39e20b378692d7d59bdc147cc
yasumuo <yasufrom24@gmail.com>
2019-02-11 22:30:56 +0900

    Deleted unnecessary function, acquired and displayed created_at

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,17 +1,4 @@
 $(function() {
-  function getFormatDate(date) {
-    var timestamp = Date.parse(date);
-    var d = new Date(timestamp);
-    var year = d.getFullYear();
-    var month = d.getMonth() + 1;
-    var day = d.getDate();
-    var hours = "0" + d.getHours();
-    var formattedHours = hours.slice(-2);
-    var minutes = d.getMinutes();
-    //var seconds = d.getSeconds();
-
-    return `${year}-${month}-${day}　${formattedHours}:${minutes}　`;
-  }
   function buildHTML(message) {
     let content = message.content ? `<div class="main__message__text">${message.content}</div>` : "";
     let img = message.image ? `<div class="main__message__image">
--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,6 @@ module ChatSpace
     end
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
-    config.time_zone = 'Asia/Tokyo'
+    config.time_zone = 'Tokyo'
   end
 end
