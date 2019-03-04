75784d06ba240fc9ed39997fbc3cc4cd9326682e
yasumuo <yasufrom24@gmail.com>
2019-03-04 18:05:00 +0900

    Fixed upload host path.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,6 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'upload-yasu-chat'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/upload-yasu-chat'
+  #config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/upload-yasu-chat'
+  config.asset_host = 'https://upload-yasu-chat.s3-ap-northeast-1.amazonaws.com'
 end
